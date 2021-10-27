aed58df9f4105ee8c37c96e6fe08c4703d2ca16d
VladyslavSovpenko <77003345+VladyslavSovpenko@users.noreply.github.com>
2021-10-27 18:32:43 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -4,3 +4,6 @@ add column salary integer;
 update developers set salary=1000 where id=1;
 update developers set salary=700 where id=4;
 update developers set salary=800 where id=5;
+update developers set salary=2000 where id=7;
+update developers set salary=900 where id=8;
+update developers set salary=200 where id=9;
