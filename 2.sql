c9e8af70639b3e4231348144e50ad9a0cf9751d8
VladyslavSovpenko <77003345+VladyslavSovpenko@users.noreply.github.com>
2021-10-27 20:43:44 +0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,7 +1,8 @@
-select p.name, sum(salary) 
+select p.name, sum(salary) --2
 from developers d
 join dev_to_project dtp on d.id = dtp.id_dev 
 join projects p on dtp.id_projects = p.id 
 group by p.id 
 order by sum(salary) desc 
+limit 1
 ;
